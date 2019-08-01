4e1c6e1517ffe6e9017eb66239903ffd3315dde8
Redaloukil <loukil.red@gmail.com>
2019-08-01 17:22:58 +0100

    Add pagination and infinite components , seperate data by pages

---
+++ frontend/src/Pages/Infinitescroll.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Infinitescroll extends React.Component {
+    state = {
+        
+    }
+    
+    render(){
+        return (
+            <div>
+                Infinitescroll Component
+            </div>
+        )
+    }
+}
+
+export default Infinitescroll;
